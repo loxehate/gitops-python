de5e34ee9d7b9c65e163f12bb53201f70e836246
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-04-01 15:22:15 +0800

    update

--- build/app.py
+++ build/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     env = os.getenv("APP_ENV", "dev")  # 默认为 dev
     if env == "prod":
-        return "Hello gitops prod date end"
+        return "Hello gitops prod email end"
     else:
         return "Hello gitops dev 1"
 
@@ -14,7 +14,7 @@ def hello():
 def api_response():
     env = os.getenv("APP_ENV", "dev")  # 默认为 dev
     if env == "prod":
-        return "api gitops prod date end"
+        return "api gitops prod email end"
     else:
         return "api gitops dev 1"
 
