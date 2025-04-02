9f853f2441026c60b89a51948b03c5669bc375cc
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-04-02 14:52:01 +0800

    update

--- build/app.py
+++ build/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 def hello():
     env = os.getenv("APP_ENV", "dev")  # 默认为 dev
     if env == "prod":
-        return "Hello gitops prod applications"
+        return "Hello gitops prod applications latest"
     else:
-        return "Hello gitops dev applications"
+        return "Hello gitops dev applications latest"
 
 @app.route('/api')
 def api_response():
     env = os.getenv("APP_ENV", "dev")  # 默认为 dev
     if env == "prod":
-        return "api gitops prod applications"
+        return "api gitops prod applications latest"
     else:
-        return "api gitops dev applications"
+        return "api gitops dev applications latest"
 
 
 if __name__ == '__main__':
